1f9ab499f7896313e91cb05c367005d8469bdc97
Stas Ukolov <ukoloff@gmail.com>
2024-12-20 14:44:35 +0500

    Quote HTML

--- src/view/hello.js
+++ src/view/hello.js
@@ -1,11 +1,12 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
+const h = require('./h')
 
 module.exports = hello
 
 async function hello(req, res) {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
   let src = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8')
-  src = src.replace('${ROOM}', decodeURIComponent(req.url).replace(/^\/+/, ''))
+  src = src.replace('${ROOM}', h(decodeURIComponent(req.url).replace(/^\/+/, '')))
   res.end(src)
 }
