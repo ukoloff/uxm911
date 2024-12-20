9acf845a1d9b1a707cd6282781c986d0873aa41b
Stas Ukolov <ukoloff@gmail.com>
2024-12-20 10:49:57 +0500

    Extract HTML

--- src/view/hello.js
+++ src/view/hello.js
@@ -1,6 +1,12 @@
+const fs = require('node:fs')
+const path = require('node:path')
+
 module.exports = hello
 
 async function hello(req, res) {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  res.end('<link href="/assets/bootswatch/spacelab/bootstrap.min.css" rel="stylesheet">Hello, world!')
+  let src = path.join(__dirname, 'index.html')
+  src = fs.createReadStream(src)
+  src
+    .on('ready', _ => src.pipe(res))
 }
