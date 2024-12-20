31cb6842a09cd532a8620cf85cd1e8a64097eb06
Stas Ukolov <ukoloff@gmail.com>
2024-12-20 13:38:38 +0500

    Fill Room

--- assets/911.js
+++ assets/911.js
@@ -2,6 +2,7 @@
   document.addEventListener("DOMContentLoaded", init, { once: true })
 
   function init() {
-
+    var f = document.forms[0]
+    f.querySelector('button').disabled = !f.room.value
   }
 }()
--- src/view/hello.js
+++ src/view/hello.js
@@ -1,12 +1,11 @@
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
 const path = require('node:path')
 
 module.exports = hello
 
 async function hello(req, res) {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
-  let src = path.join(__dirname, 'index.html')
-  src = fs.createReadStream(src)
-  src
-    .on('ready', _ => src.pipe(res))
+  let src = await fs.readFile(path.join(__dirname, 'index.html'), 'utf-8')
+  src = src.replace('${ROOM}', decodeURIComponent(req.url).replace(/^\/+/, ''))
+  res.end(src)
 }
