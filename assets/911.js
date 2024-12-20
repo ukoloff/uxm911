3e0bcf5cca51cf750cc5dd2e9c446d315c9afdf0
Stas Ukolov <ukoloff@gmail.com>
2024-12-20 13:55:50 +0500

    POST JSON

--- assets/911.js
+++ assets/911.js
@@ -3,6 +3,19 @@
 
   function init() {
     var f = document.forms[0]
+    f.onsubmit = send
     f.querySelector('button').disabled = !f.room.value
+  }
+
+  async function send(ev) {
+    ev.preventDefault()
+    this.querySelector('button').disabled = true
+    await fetch('/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(Object.fromEntries(new FormData(this)))
+    })
   }
 }()
