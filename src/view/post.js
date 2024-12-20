f15fee5445db7079efece1ab7c1328b1af47567d
Stas Ukolov <ukoloff@gmail.com>
2024-12-20 14:39:40 +0500

    Get POST data

--- src/view/post.js
+++ src/view/post.js
@@ -1,5 +1,22 @@
 module.exports = post
 
 async function post(req, res) {
-  res.end()
+  try {
+    var data = await serve(req)
+  } catch (e) {
+    data = {
+      error: e.name,
+      message: e.message
+    }
+  }
+  res.setHeader('Content-Type', 'application/json')
+  res.end(JSON.stringify(data))
+}
+
+async function serve(req) {
+  var data = ''
+  for await (const chunk of req) {
+    data += chunk
+  }
+  return JSON.parse(data)
 }
